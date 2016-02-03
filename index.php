0c6c504d539d3acabc070e2d0cc03b8016590102
rvabre <romain.vabre.pro@gmail.com>
2016-02-03 14:45:50 +0100

    Update index.php

--- index.php
+++ index.php
@@ -2,14 +2,8 @@
    echo 'Hello ALTRAN';
    echo "</br>";
    for ($i=0; $i<100; $i++) {
-      
-      if ($i%3==0){
-         echo "C";
-      }else if ($i%2==0){
-         echo "B";
-      }else{
-         echo "A";
-      }
+   echo "Altran";
+   echo "</br>";
 }
 
 ?>
