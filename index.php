2b174ae118a29bf9d3799c688dfac5f331acfd31
rvabre <romain.vabre.pro@gmail.com>
2016-02-03 14:33:53 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,16 +1,8 @@
 <?php
    echo 'Hello ALTRAN';
+   echo "</br>";
    for ($i=0; $i<100; $i++) {
     echo 'A';
 }
-echo "</br>";
-echo"&nbsp;$$$$$$\&nbsp;&nbsp;$$\&nbsp;&nbsp;&nbsp;&nbsp;$$$$$$$$\&nbsp;$$$$$$$\&nbsp;&nbsp;&nbsp;$$$$$$\&nbsp;&nbsp;$$\&nbsp;&nbsp;&nbsp;$$\&nbsp;</br>";
-echo"$$&nbsp;&nbsp;__$$\&nbsp;$$&nbsp;|&nbsp;&nbsp;&nbsp;\__$$&nbsp;&nbsp;__|$$&nbsp;&nbsp;__$$\&nbsp;$$&nbsp;&nbsp;__$$\&nbsp;$$$\&nbsp;&nbsp;$$&nbsp;|</br>";
-echo"$$&nbsp;/&nbsp;&nbsp;$$&nbsp;|$$&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;$$&nbsp;|&nbsp;&nbsp;&nbsp;$$&nbsp;|&nbsp;&nbsp;$$&nbsp;|$$&nbsp;/&nbsp;&nbsp;$$&nbsp;|$$$$\&nbsp;$$&nbsp;|</br>";
-echo"$$$$$$$$&nbsp;|$$&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;$$&nbsp;|&nbsp;&nbsp;&nbsp;$$$$$$$&nbsp;&nbsp;|$$$$$$$$&nbsp;|$$&nbsp;$$\$$&nbsp;|</br>";
-echo"$$&nbsp;&nbsp;__$$&nbsp;|$$&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;$$&nbsp;|&nbsp;&nbsp;&nbsp;$$&nbsp;&nbsp;__$$<&nbsp;$$&nbsp;&nbsp;__$$&nbsp;|$$&nbsp;\$$$$&nbsp;|</br>";
-echo"$$&nbsp;|&nbsp;&nbsp;$$&nbsp;|$$&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;$$&nbsp;|&nbsp;&nbsp;&nbsp;$$&nbsp;|&nbsp;&nbsp;$$&nbsp;|$$&nbsp;|&nbsp;&nbsp;$$&nbsp;|$$&nbsp;|\$$$&nbsp;|</br>";
-echo"$$&nbsp;|&nbsp;&nbsp;$$&nbsp;|$$$$$$$$\&nbsp;$$&nbsp;|&nbsp;&nbsp;&nbsp;$$&nbsp;|&nbsp;&nbsp;$$&nbsp;|$$&nbsp;|&nbsp;&nbsp;$$&nbsp;|$$&nbsp;|&nbsp;\$$&nbsp;|</br>";
-echo"\__|&nbsp;&nbsp;\__|\________|\__|&nbsp;&nbsp;&nbsp;\__|&nbsp;&nbsp;\__|\__|&nbsp;&nbsp;\__|\__|&nbsp;&nbsp;\__|</br>";
 
 ?>
