e8fc15b12ae41cff343768b946dcb1683c932231
rvabre <romain.vabre.pro@gmail.com>
2016-02-03 15:13:30 +0100

    Update index.php

--- index.php
+++ index.php
@@ -7,15 +7,22 @@
 
 
 }
- $var = getenv('OPENSHIFT_MYSQL_DB_HOST');
-  echo " $var</br>";
-   $var = getenv('OPENSHIFT_MYSQL_DB_USERNAME'); 
-    echo " $var</br>";
-    $var = getenv('OPENSHIFT_MYSQL_DB_HOST');
-     echo " $var</br>";
-    $var = getenv('OPENSHIFT_MYSQL_DB_PASSWORD');
-     echo " $var</br>";
-    $var = getenv('OPENSHIFT_MYSQL_DB_PORT');
-     echo " $var</br>";
+define('DB_HOST', getenv('OPENSHIFT_MYSQL_DB_HOST'));
+define('DB_PORT', getenv('OPENSHIFT_MYSQL_DB_PORT'));
+define('DB_USER', getenv('OPENSHIFT_MYSQL_DB_USERNAME'));
+define('DB_PASS', getenv('OPENSHIFT_MYSQL_DB_PASSWORD'));
+define('DB_NAME', getenv('OPENSHIFT_GEAR_NAME'));
+
+$dbhost = constant("DB_HOST"); // Host name 
+$dbport = constant("DB_PORT"); // Host port
+$dbusername = constant("DB_USER"); // Mysql username 
+$dbpassword = constant("DB_PASS"); // Mysql password 
+$db_name = constant("DB_NAME");
+
+echo $dbhost;
+echo $dbport;
+echo $dbusername;
+echo $dbpassword;
+echo $db_name;
     
 ?>
