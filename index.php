3e85ab8e2b5744b817f9096424ecff8e5dd071a9
rvabre <romain.vabre.pro@gmail.com>
2016-02-03 15:17:08 +0100

    Update index.php

--- index.php
+++ index.php
@@ -7,20 +7,16 @@
 
 
 }
-define('DB_HOST', getenv('OPENSHIFT_MYSQL_DB_HOST'));
-define('DB_PORT', getenv('OPENSHIFT_MYSQL_DB_PORT'));
-define('DB_USER', getenv('OPENSHIFT_MYSQL_DB_USERNAME'));
-define('DB_PASS', getenv('OPENSHIFT_MYSQL_DB_PASSWORD'));
-define('DB_NAME', getenv('OPENSHIFT_GEAR_NAME'));
 
-$dbhost = constant("DB_HOST"); // Host name 
-$dbport = constant("DB_PORT"); // Host port
+define('DB_USER', getenv('MYSQL_USER'));
+define('DB_PASS', getenv('MYSQL_PASSWORD'));
+define('DB_NAME', getenv('MYSQL_DATABASE'));
+
+
 $dbusername = constant("DB_USER"); // Mysql username 
 $dbpassword = constant("DB_PASS"); // Mysql password 
 $db_name = constant("DB_NAME");
 
-echo $dbhost;
-echo $dbport;
 echo $dbusername;
 echo $dbpassword;
 echo $db_name;
